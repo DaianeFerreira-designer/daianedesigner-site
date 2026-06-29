65f08aba4ebfc477b89083bac565f40da81313b7
Daiane Ferreira <daianeferreira.agencia@gmail.com>
2026-06-29 15:44:55 -0300

    ajustes

--- partials/header.php
+++ partials/header.php
@@ -1,16 +1,10 @@
-<header>
+<header class="headerDesktop">
   <a href="/">
     <img
       class="logo"
       src="/assets/imagens/logo-daiane-ferreira.svg"
       alt="Logo Daiane Ferreira" />
   </a>
-  
-<button class="menuMobileBtn" type="button" aria-label="Abrir menu">
-    <span></span>
-    <span></span>
-    <span></span>
-  </button>
 
   <nav class="principal">
     <ul>
@@ -27,4 +21,59 @@
     class="icone"
     src="/assets/imagens/icon-daiane-ferreira-logo-web-design-sites.svg"
     alt="Ícone Daiane Ferreira" />
+</header>
+
+<header class="headerMobile">
+  <a href="/">
+    <img class="logoMobile" src="/assets/imagens/logo-daiane-ferreira.svg" alt="Logo Daiane Ferreira" />
+  </a>
+
+  <button class="menuMobileToggle" type="button" aria-label="Abrir menu">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+
+  <nav class="menuMobileDrop">
+    <ul>
+      <li>
+        <a href="/">
+          <span class="menuIcon">⌂</span>
+          <span>Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="/criacao-de-sites-profissionais">
+          <span class="menuIcon">◈</span>
+          <span>Serviços</span>
+        </a>
+      </li>
+      <li>
+        <a href="/templates-elementor-wordpress-figma">
+          <span class="menuIcon">◇</span>
+          <span>Templates</span>
+        </a>
+      </li>
+      <li>
+        <a href="/portfolio-sites-profissionais">
+          <span class="menuIcon">⌘</span>
+          <span>Portfólio</span>
+        </a>
+      </li>
+      <li>
+        <a href="/sobre-daiane-ferreira-web-designer">
+          <span class="menuIcon">♙</span>
+          <span>Sobre</span>
+        </a>
+      </li>
+      <li>
+        <a href="/contato-criacao-de-sites">
+          <span class="menuIcon">✉</span>
+          <span>Contato</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+  <img class="iconeMobile" src="/assets/imagens/icon-daiane-ferreira-logo-web-design-sites.svg" alt="Ícone Daiane Ferreira" />
 </header>
